e7285e4bfebb90be30d6f1d84f0c6d6d98e8b7ba
cym <cym@google.com>
2018-03-27 11:42:23 -0700

    Add C++ and Python support for reading BED files.
    PiperOrigin-RevId: 190647305

---
+++ deepvariant/util/io/bed_test.py
@@ -0,0 +1,68 @@
+# Copyright 2018 Google Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""Tests for deepvariant.util.io.bed."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+from absl.testing import absltest
+from absl.testing import parameterized
+
+from deepvariant.util.io import bed
+from deepvariant.util.genomics import bed_pb2
+from deepvariant.util import test_utils
+
+
+class BedReaderTests(parameterized.TestCase):
+
+  @parameterized.parameters('test_regions.bed', 'test_regions.bed.gz',
+                            'test_regions.bed.tfrecord',
+                            'test_regions.bed.tfrecord.gz')
+  def test_iterate_bed_reader(self, bed_filename):
+    bed_path = test_utils.genomics_core_testdata(bed_filename)
+    expected = [('chr1', 10, 20), ('chr1', 100, 200)]
+    with bed.BedReader(bed_path) as reader:
+      records = list(reader.iterate())
+    self.assertLen(records, 2)
+    self.assertEqual([(r.reference_name, r.start, r.end) for r in records],
+                     expected)
+
+  @parameterized.parameters('test_regions.bed', 'test_regions.bed.gz')
+  def test_native_bed_header(self, bed_filename):
+    bed_path = test_utils.genomics_core_testdata(bed_filename)
+    with bed.BedReader(bed_path) as reader:
+      self.assertEqual(reader.header.num_fields, 12)
+    with bed.NativeBedReader(bed_path) as native_reader:
+      self.assertEqual(native_reader.header.num_fields, 12)
+
+
+if __name__ == '__main__':
+  absltest.main()
