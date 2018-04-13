a2ede3248836d64a2fc5909b4d4e9465e3ff63d1
mdepristo <mdepristo@google.com>
2018-04-13 15:13:56 -0700

    Cleanup API and add extensive tests to window_selector.py in preparation for future optimizations.
    PiperOrigin-RevId: 192831444

--- deepvariant/realigner/window_selector.py
+++ deepvariant/realigner/window_selector.py
@@ -35,217 +35,244 @@ from __future__ import print_function
 from collections import defaultdict
 
 from third_party.nucleus.protos import cigar_pb2
-from third_party.nucleus.protos import range_pb2
+from third_party.nucleus.util import ranges
 from deepvariant.realigner import utils
 
 
-class WindowSelector(object):
-  """Provides a set of candidate genomic windows for local assembly."""
+def _process_align_match(config, cigar, ref, read, ref_pos, read_pos):
+  for _ in range(cigar.operation_length):
+    # Check for ref_pos ranges, if the read is partially overlapped with
+    # ref sequence, it might be out-of-range and result in index error.
+    if ref_pos >= len(ref):
+      break
+    if ref_pos >= 0:
+      if ref[ref_pos] != read.aligned_sequence[read_pos]:
+        if read.aligned_quality[read_pos] >= config.min_base_quality:
+          yield ref_pos
+    read_pos += 1
+    ref_pos += 1
 
-  def __init__(self, config):
-    """Initializations.
 
-    Args:
-      config: Realigner.RealignerOptions.WindowSelectorOptions record.
-    """
-    self.config = config
-
-  def _process_align_match(self, cigar, ref, read, ref_pos, read_pos):
-    for _ in range(cigar.operation_length):
-      # Check for ref_pos ranges, if the read is partially overlapped with
-      # ref sequence, it might be out-of-range and result in index error.
-      if ref_pos >= len(ref):
-        break
-      if ref_pos >= 0:
-        if ref[ref_pos] != read.aligned_sequence[read_pos]:
-          if read.aligned_quality[read_pos] >= self.config.min_base_quality:
-            yield ref_pos
-      read_pos += 1
-      ref_pos += 1
-
-  def _process_seq_mismatch(self, cigar, ref, read, ref_pos, read_pos):
-    del ref  # Unused in processing cigar seq_mismatch operation.
-    for _ in range(cigar.operation_length):
-      if read.aligned_quality[read_pos] >= self.config.min_base_quality:
-        yield ref_pos
-      read_pos += 1
-      ref_pos += 1
-
-  def _process_insert(self, cigar, ref, read, ref_pos, read_pos):
-    del ref  # Unused in processing cigar insert operation.
-    for i in range(cigar.operation_length):
-      if read.aligned_quality[read_pos] >= self.config.min_base_quality:
-        yield ref_pos + i
-        yield ref_pos - cigar.operation_length + i
-      read_pos += 1
-
-  def _process_soft_clip(self, cigar, ref, read, ref_pos, read_pos):
-    del ref  # Unused in processing cigar soft_clip operation.
-    if ref_pos == read.alignment.position.position:
-      offset = -cigar.operation_length
-    else:
-      offset = 0
-    for i in range(cigar.operation_length):
-      if read.aligned_quality[read_pos] >= self.config.min_base_quality:
-        yield ref_pos + offset + i
-      read_pos += 1
-
-  def _process_delete(self, cigar, ref, read, ref_pos, read_pos):
-    del ref, read, read_pos  # Unused in processing cigar delete operation.
-    for _ in range(cigar.operation_length):
+def _process_seq_mismatch(config, cigar, ref, read, ref_pos, read_pos):
+  del ref  # Unused in processing cigar seq_mismatch operation.
+  for _ in range(cigar.operation_length):
+    if read.aligned_quality[read_pos] >= config.min_base_quality:
       yield ref_pos
-      ref_pos += 1
+    read_pos += 1
+    ref_pos += 1
 
-  def process_read(self, ref, read, ref_offset=0):
-    """Yields reference positions corresponding to read's variations.
 
-    Following cigar operations generate candidate position:
-      - ALIGNMENT_MATCH: at mismatch positions
-      - SEQUENCE_MISMATCH: at positions within
-          [cigar_start, cigar_start + cigar_len)
-      - DELETE: at positions within [cigar_start, cigar_start + cigar_len)
-      - INSERT: at positions within
-          [cigar_start - cigar_len, cigar_start + cigar_len)
-      - CLIP_SOFT: at positions within [cigar_start, cigar_start + cigar_len)
-      - SKIP: at positions within [cigar_start, cigar_start + cigar_len)
+def _process_insert(config, cigar, ref, read, ref_pos, read_pos):
+  del ref  # Unused in processing cigar insert operation.
+  for i in range(cigar.operation_length):
+    if read.aligned_quality[read_pos] >= config.min_base_quality:
+      yield ref_pos + i
+      yield ref_pos - cigar.operation_length + i
+    read_pos += 1
 
-    Following filters are applied:
-     - A read with low-quality alignment score is ignored.
-     - A variation position corresponding to a low quality base is ignored.
 
-    Args:
-      ref: reference sequence
-      read: A genomics.Read record.
-      ref_offset: Start offset for reference position.
+def _process_soft_clip(config, cigar, ref, read, ref_pos, read_pos):
+  del ref  # Unused in processing cigar soft_clip operation.
+  if ref_pos == read.alignment.position.position:
+    offset = -cigar.operation_length
+  else:
+    offset = 0
+  for i in range(cigar.operation_length):
+    if read.aligned_quality[read_pos] >= config.min_base_quality:
+      yield ref_pos + offset + i
+    read_pos += 1
 
-    Yields:
-      reference positions within range of [0, seq_len], corresponding
-      to read's variations.
 
-    Raises:
-      ValueError: for unsupported cigar operation.
-    """
+def _process_delete(config, cigar, ref, read, ref_pos, read_pos):
+  # Unused in processing cigar delete operation.
+  del config, ref, read, read_pos
+  for _ in range(cigar.operation_length):
+    yield ref_pos
+    ref_pos += 1
 
-    if read.alignment.mapping_quality < self.config.min_mapq:
-      return
 
-    ref_pos = read.alignment.position.position - ref_offset
-    read_pos = 0
-    # Use set(), as some cigar operations might generate duplicated positions,
-    # E.g. for insertions, it extends the candidate positions to
-    # [ins_pos - ins_len, ins_pos + ins_len] which might overlap with some
-    # nearby mismatches.
-    positions = set()
-    for cigar in read.alignment.cigar:
-      # Break if it reached the end of reference sequence.
-      if ref_pos >= len(ref):
-        break
-      if cigar.operation not in utils.CIGAR_OPS:
-        raise ValueError('Unexpected CIGAR operation', cigar, read)
+def _process_read(config, ref, read, ref_offset):
+  """Yields reference positions corresponding to read's variations.
 
-      if cigar.operation == cigar_pb2.CigarUnit.ALIGNMENT_MATCH:
-        positions.update(
-            self._process_align_match(cigar, ref, read, ref_pos, read_pos))
-        read_pos += cigar.operation_length
-        ref_pos += cigar.operation_length
-      elif cigar.operation == cigar_pb2.CigarUnit.SEQUENCE_MISMATCH:
-        positions.update(
-            self._process_seq_mismatch(cigar, ref, read, ref_pos, read_pos))
-        read_pos += cigar.operation_length
-        ref_pos += cigar.operation_length
-      elif cigar.operation == cigar_pb2.CigarUnit.INSERT:
-        positions.update(
-            self._process_insert(cigar, ref, read, ref_pos, read_pos))
-        read_pos += cigar.operation_length
-      elif cigar.operation == cigar_pb2.CigarUnit.CLIP_SOFT:
-        positions.update(
-            self._process_soft_clip(cigar, ref, read, ref_pos, read_pos))
-        read_pos += cigar.operation_length
-      elif (cigar.operation == cigar_pb2.CigarUnit.DELETE or
-            cigar.operation == cigar_pb2.CigarUnit.SKIP):
-        positions.update(
-            self._process_delete(cigar, ref, read, ref_pos, read_pos))
-        ref_pos += cigar.operation_length
-      elif cigar.operation == cigar_pb2.CigarUnit.SEQUENCE_MATCH:
-        ref_pos += cigar.operation_length
-        read_pos += cigar.operation_length
-      elif (cigar.operation == cigar_pb2.CigarUnit.CLIP_HARD or
-            cigar.operation == cigar_pb2.CigarUnit.PAD):
-        pass
+  See _candidates_from_reads for more details on this algorithm.
 
-    # Yield positions within the range
-    for pos in sorted(positions):
-      if pos >= 0 and pos < len(ref):
-        yield pos
+  Args:
+    config: learning.genomics.deepvariant.realigner.WindowSelectorOptions
+      options determining the behavior of this window selector.
+    ref: reference sequence
+    read: A genomics.Read record.
+    ref_offset: Start offset for reference position.
 
-  def windows(self, candidate_pos, ref_name, ref_offset):
-    """"Process candidate positions to determine windows for local assembly.
+  Yields:
+    reference positions within range of [0, seq_len], corresponding
+    to read's variations.
 
-    Following filters are applied:
-      - Candidate position with low number of supporting reads is ignored.
-      - Candidate position with too many of supporting reads is ignored.
+  Raises:
+    ValueError: for unsupported cigar operation.
+  """
 
-    Windows are within range of
-      [min(pos) - self.config.min_windows_distance,
-       max(pos) + self.config.min_windows_distance)
+  if read.alignment.mapping_quality < config.min_mapq:
+    return
 
-    Args:
-      candidate_pos: A dictionary with ref_pos as key and number of supporting
-        reads as its value.
-      ref_name: Reference name, used in setting the output
-        genomics.range.reference_name value.
-      ref_offset: Start offset for reference position.
+  ref_pos = read.alignment.position.position - ref_offset
+  read_pos = 0
+  # Use set(), as some cigar operations might generate duplicated positions,
+  # E.g. for insertions, it extends the candidate positions to
+  # [ins_pos - ins_len, ins_pos + ins_len] which might overlap with some
+  # nearby mismatches.
+  positions = set()
+  for cigar in read.alignment.cigar:
+    # Break if it reached the end of reference sequence.
+    if ref_pos >= len(ref):
+      break
+    if cigar.operation not in utils.CIGAR_OPS:
+      raise ValueError('Unexpected CIGAR operation', cigar, read)
 
-    Yields:
-      A genomics.range record. Note: only start and end fields are
-      populated.
-    """
+    if cigar.operation == cigar_pb2.CigarUnit.ALIGNMENT_MATCH:
+      positions.update(
+          _process_align_match(config, cigar, ref, read, ref_pos, read_pos))
+      read_pos += cigar.operation_length
+      ref_pos += cigar.operation_length
+    elif cigar.operation == cigar_pb2.CigarUnit.SEQUENCE_MISMATCH:
+      positions.update(
+          _process_seq_mismatch(config, cigar, ref, read, ref_pos, read_pos))
+      read_pos += cigar.operation_length
+      ref_pos += cigar.operation_length
+    elif cigar.operation == cigar_pb2.CigarUnit.INSERT:
+      positions.update(
+          _process_insert(config, cigar, ref, read, ref_pos, read_pos))
+      read_pos += cigar.operation_length
+    elif cigar.operation == cigar_pb2.CigarUnit.CLIP_SOFT:
+      positions.update(
+          _process_soft_clip(config, cigar, ref, read, ref_pos, read_pos))
+      read_pos += cigar.operation_length
+    elif (cigar.operation == cigar_pb2.CigarUnit.DELETE or
+          cigar.operation == cigar_pb2.CigarUnit.SKIP):
+      positions.update(
+          _process_delete(config, cigar, ref, read, ref_pos, read_pos))
+      ref_pos += cigar.operation_length
+    elif cigar.operation == cigar_pb2.CigarUnit.SEQUENCE_MATCH:
+      ref_pos += cigar.operation_length
+      read_pos += cigar.operation_length
+    elif (cigar.operation == cigar_pb2.CigarUnit.CLIP_HARD or
+          cigar.operation == cigar_pb2.CigarUnit.PAD):
+      pass
 
-    start_pos = end_pos = -1
-    for pos in sorted(candidate_pos):
-      if candidate_pos[pos] < self.config.min_num_supporting_reads:
-        continue
-      if candidate_pos[pos] > self.config.max_num_supporting_reads:
-        continue
-      if start_pos == -1:
-        start_pos = pos
-        end_pos = pos
-      elif pos > end_pos + self.config.min_windows_distance:
-        yield range_pb2.Range(
-            reference_name=ref_name,
-            start=start_pos + ref_offset - self.config.min_windows_distance,
-            end=end_pos + ref_offset + self.config.min_windows_distance)
-        start_pos = pos
-        end_pos = pos
-      else:
-        end_pos = pos
-    if start_pos != -1:
-      yield range_pb2.Range(
-          reference_name=ref_name,
-          start=start_pos + ref_offset - self.config.min_windows_distance,
-          end=end_pos + ref_offset + self.config.min_windows_distance)
+  # Yield positions within the range
+  for pos in sorted(positions):
+    if pos >= 0 and pos < len(ref):
+      yield pos + ref_offset
 
-  def process_reads(self, ref, reads, ref_name, ref_offset):
-    """"Process reads to determine candidate windows for local assembly.
 
-    Windows are within range of
-      [0 - self.config.min_windows_distance,
-       ref_len + self.config.min_windows_distance)
+def _candidates_from_reads(config, ref, reads, region):
+  """Returns a dictionary mapping positions to non-reference counts.
 
-    Args:
-      ref: reference sequence.
-      reads: A list of genomics.Read records.
-      ref_name: Reference name, used in setting output
-        genomics.range.reference_name values.
-      ref_offset: Start offset for reference position.
+  Following cigar operations generate candidate position:
+    - ALIGNMENT_MATCH: at mismatch positions
+    - SEQUENCE_MISMATCH: at positions within
+        [cigar_start, cigar_start + cigar_len)
+    - DELETE: at positions within [cigar_start, cigar_start + cigar_len)
+    - INSERT: at positions within
+        [cigar_start - cigar_len, cigar_start + cigar_len)
+    - CLIP_SOFT: at positions within [cigar_start, cigar_start + cigar_len)
+    - SKIP: at positions within [cigar_start, cigar_start + cigar_len)
 
-    Returns:
-      A list of genomics.range records.
-    """
-    # A list of candidate positions mapping to their number of supporting reads.
-    candidates = defaultdict(int)
+  Following filters are applied:
+   - A read with low-quality alignment score is ignored.
+   - A variation position corresponding to a low quality base is ignored.
 
-    for read in reads:
-      for ref_pos in self.process_read(ref, read, ref_offset):
-        candidates[ref_pos] += 1
-    return self.windows(candidates, ref_name, ref_offset)
+  Args:
+    config: learning.genomics.deepvariant.realigner.WindowSelectorOptions
+      options determining the behavior of this window selector.
+    ref: str. The reference bases as a string, where the first character of
+      ref starts at region.start and the last position in region.end.
+    reads: list[nucleus.protos.Read]. The reads we are processing into
+      candidate positions.
+    region: nucleus.protos.Range. The region we are processing.
+
+  Returns:
+    A dictionary. The keys are reference positions within region. The value of
+    each key is the number of candidate observations (see above) seen at that
+    position summed up over all reads. The dictionary doesn't contain any
+    position keys that would have a value of 0 (i.e., it's sparse).
+  """
+  # A list of candidate positions mapping to their number of supporting reads.
+  candidates = defaultdict(int)
+
+  for read in reads:
+    for ref_pos in _process_read(config, ref, read, region.start):
+      candidates[ref_pos] += 1
+
+  return candidates
+
+
+def _candidates_to_windows(config, candidate_pos, ref_name):
+  """"Process candidate positions to determine windows for local assembly.
+
+  Following filters are applied:
+    - Candidate position with low number of supporting reads is ignored.
+    - Candidate position with too many of supporting reads is ignored.
+
+  Windows are within range of
+    [min(pos) - config.min_windows_distance,
+     max(pos) + config.min_windows_distance)
+
+  Args:
+    config: learning.genomics.deepvariant.realigner.WindowSelectorOptions
+      options determining the behavior of this window selector.
+    candidate_pos: A dictionary with ref_pos as key and number of supporting
+      reads as its value.
+    ref_name: Reference name, used in setting the output
+      genomics.range.reference_name value.
+
+  Returns:
+    A sorted list of nucleus.protos.Range protos for all windows in this region.
+  """
+  windows = []
+
+  def _add_window(start_pos, end_pos):
+    windows.append(
+        ranges.make_range(ref_name, start_pos - config.min_windows_distance,
+                          end_pos + config.min_windows_distance))
+
+  start_pos, end_pos = None, None
+  for pos, count in sorted(candidate_pos.iteritems()):
+    if count < config.min_num_supporting_reads:
+      continue
+    if count > config.max_num_supporting_reads:
+      continue
+    if start_pos is None:
+      start_pos = pos
+      end_pos = pos
+    elif pos > end_pos + config.min_windows_distance:
+      _add_window(start_pos, end_pos)
+      start_pos = pos
+      end_pos = pos
+    else:
+      end_pos = pos
+  if start_pos is not None:
+    _add_window(start_pos, end_pos)
+
+  return sorted(windows, key=ranges.as_tuple)
+
+
+# redacted
+def select_windows(config, ref_reader, reads, region):
+  """"Process reads to determine candidate windows for local assembly.
+
+  Windows are within range of
+    [0 - config.min_windows_distance, ref_len + config.min_windows_distance)
+
+  Args:
+    config: learning.genomics.deepvariant.realigner.WindowSelectorOptions
+      options determining the behavior of this window selector.
+    ref_reader: GenomeReference. Indexed reference genome to query bases.
+    reads: A list of genomics.Read records.
+    region: nucleus.protos.Range. The region we are processing.
+
+  Returns:
+    A list of nucleus.protos.Range protos sorted by their genomic position.
+  """
+  ref = ref_reader.query(region)
+  candidates = _candidates_from_reads(config, ref, reads, region)
+  return _candidates_to_windows(config, candidates, region.reference_name)
--- deepvariant/realigner/window_selector_test.py
+++ deepvariant/realigner/window_selector_test.py
@@ -35,85 +35,186 @@ from __future__ import print_function
 
 
 from absl.testing import absltest
-
-from third_party.nucleus.protos import range_pb2
+from absl.testing import parameterized
 
 from third_party.nucleus.testing import test_utils
+from third_party.nucleus.util import ranges
 from deepvariant.protos import realigner_pb2
-from deepvariant.realigner.window_selector import WindowSelector
+from deepvariant.realigner import window_selector
 
 
-class WindowSelectorTest(absltest.TestCase):
+class WindowSelectorTest(parameterized.TestCase):
 
-  def test_ws_config(self):
-    return realigner_pb2.RealignerOptions.WindowSelectorOptions(
+  def setUp(self):
+    self.config = realigner_pb2.RealignerOptions.WindowSelectorOptions(
         min_num_supporting_reads=2,
         max_num_supporting_reads=10,
         min_mapq=20,
         min_base_quality=20,
         min_windows_distance=4)
 
-  def test_process_read(self):
-    """Test WindowSelector.process_read()."""
-    window = WindowSelector(self.test_ws_config())
-
-    ref = 'A' * 100
-
-    read_1 = test_utils.make_read(
-        'AAGA', start=10, cigar='4M', quals=[64] * 4, name='read_1')
-    read_2 = test_utils.make_read(
-        'AAGTA', start=10, cigar='2M2I1M', quals=[64] * 5, name='read_2')
-    read_3 = test_utils.make_read(
-        'AAA', start=10, cigar='2M2D1M', quals=[64] * 3, name='read_3')
-    read_4 = test_utils.make_read(
-        'TGATAC', start=10, cigar='2S3M1S', quals=[64] * 6, name='read_4')
-    read_5 = test_utils.make_read(
-        'AAGA', start=10, cigar='2M1X1M', quals=[64] * 4, name='read_5')
-
-    self.assertEqual(list(window.process_read(ref, read_1)), [12])
-    self.assertEqual(list(window.process_read(ref, read_2)), [10, 11, 12, 13])
-    self.assertEqual(list(window.process_read(ref, read_3)), [12, 13])
-    self.assertEqual(list(window.process_read(ref, read_4)), [8, 9, 11, 13])
-    self.assertEqual(list(window.process_read(ref, read_5)), [12])
-
-  def test_candidate_pos_low_qual(self):
+  @parameterized.parameters(
+      # ------------------------------------------------------------------------
+      # These reads are all simple and just test the basic position calculation.
+      # ------------------------------------------------------------------------
+      dict(
+          read=test_utils.make_read(
+              'AAGA', start=10, cigar='4M', quals=[64] * 4),
+          expected_candidate_positions=[12]),
+      dict(
+          read=test_utils.make_read(
+              'AAGTA', start=10, cigar='2M2I1M', quals=[64] * 5),
+          expected_candidate_positions=[10, 11, 12, 13]),
+      dict(
+          read=test_utils.make_read(
+              'AAA', start=10, cigar='2M2D1M', quals=[64] * 3),
+          expected_candidate_positions=[12, 13]),
+      dict(
+          read=test_utils.make_read(
+              'TGATAC', start=10, cigar='2S3M1S', quals=[64] * 6),
+          expected_candidate_positions=[8, 9, 11, 13]),
+      dict(
+          read=test_utils.make_read(
+              'AAGA', start=10, cigar='2M1X1M', quals=[64] * 4),
+          expected_candidate_positions=[12]),
+      # ------------------------------------------------------------------------
+      # These reads test that we correctly ignore bases with low qualities.
+      # ------------------------------------------------------------------------
+      dict(
+          read=test_utils.make_read(
+              'AAGA', start=10, cigar='4M', quals=[64, 64, 10, 30]),
+          expected_candidate_positions=[]),
+      dict(
+          read=test_utils.make_read(
+              'AAGTA', start=10, cigar='2M2I1M', quals=[64, 64, 10, 30, 64]),
+          expected_candidate_positions=[11, 13]),
+      dict(
+          read=test_utils.make_read(
+              'TGATAC',
+              start=10,
+              cigar='2S3M1S',
+              quals=[64, 10, 64, 64, 64, 64]),
+          expected_candidate_positions=[8, 11, 13]),
+      dict(
+          read=test_utils.make_read(
+              'AAGA', start=10, cigar='2M1X1M', quals=[64, 64, 30, 10]),
+          expected_candidate_positions=[12]),
+  )
+  def test_candidates_from_one_read(self, read, expected_candidate_positions):
     """Test WindowSelector.process_read() with reads of low quality."""
-    window = WindowSelector(self.test_ws_config())
-
-    ref = 'A' * 100
-
-    read_1 = test_utils.make_read(
-        'AAGA', start=10, cigar='4M', quals=[64, 64, 10, 30], name='read_1')
-    read_2 = test_utils.make_read(
-        'AAGTA',
-        start=10,
-        cigar='2M2I1M',
-        quals=[64, 64, 10, 30, 64],
-        name='read_2')
-    read_3 = test_utils.make_read(
-        'TGATAC',
-        start=10,
-        cigar='2S3M1S',
-        quals=[64, 10, 64, 64, 64, 64],
-        name='read_3')
-    read_4 = test_utils.make_read(
-        'AAGA', start=10, cigar='2M1X1M', quals=[64, 64, 30, 10], name='read_4')
-
-    self.assertEqual(list(window.process_read(ref, read_1)), [])
-    self.assertEqual(list(window.process_read(ref, read_2)), [11, 13])
-    self.assertEqual(list(window.process_read(ref, read_3)), [8, 11, 13])
-    self.assertEqual(list(window.process_read(ref, read_4)), [12])
-
-  def test_windows(self):
-    """Test WindowSelector.windows()."""
-    window = WindowSelector(self.test_ws_config())
-    candidates = {0: 2, 2: 4, 3: 11, 8: 3}
-
+    region = ranges.make_range(read.alignment.position.reference_name, 0, 100)
+    ref = 'A' * ranges.length(region)
     self.assertEqual(
-        list(window.windows(candidates, 'ref', 0)), [
-            range_pb2.Range(reference_name='ref', start=-4, end=6),
-            range_pb2.Range(reference_name='ref', start=4, end=12)
-        ])
+        window_selector._candidates_from_reads(self.config, ref, [read],
+                                               region),
+        {pos: 1 for pos in expected_candidate_positions})
+
+  @parameterized.parameters(
+      # If we have no candidates, we have no windows to assemble.
+      dict(candidates={}, expected_ranges=[]),
+      # Our min count for candidates is 2, so we don't get any ranges back when
+      # we have only a single candidate with a count of 1.
+      dict(candidates={4: 1}, expected_ranges=[]),
+      # Our max count for candidates is 10, so we don't get any ranges back when
+      # we have only a single candidate with a count of 11.
+      dict(candidates={4: 11}, expected_ranges=[]),
+      # Check that this works with 2 isolated regions.
+      dict(
+          candidates={
+              100: 5,
+              200: 5,
+          },
+          expected_ranges=[
+              ranges.make_range('ref', 96, 104),
+              ranges.make_range('ref', 196, 204),
+          ]),
+      # Check that this works with 3 isolated regions.
+      dict(
+          candidates={
+              100: 5,
+              200: 5,
+              300: 5,
+          },
+          expected_ranges=[
+              ranges.make_range('ref', 96, 104),
+              ranges.make_range('ref', 196, 204),
+              ranges.make_range('ref', 296, 304),
+          ]),
+      # redacted
+      # redacted
+      # Check a simple example where we have candidates from two regions:
+      dict(
+          candidates={
+              0: 2,
+              2: 4,
+              3: 11,
+              8: 3
+          },
+          expected_ranges=[
+              ranges.make_range('ref', -4, 6),
+              ranges.make_range('ref', 4, 12),
+          ]),
+  )
+  def test_candidates_to_windows(self, candidates, expected_ranges):
+    self.assertEqual(
+        window_selector._candidates_to_windows(self.config, candidates, 'ref'),
+        expected_ranges)
+
+  @parameterized.parameters(range(1, 20))
+  def test_candidates_to_windows_window_size(self, size):
+    # We have a single candidate at position 100 with a 5 count.
+    candidates = {100: 5}
+    # We expect the created window to be +/- size from 100.
+    expected = ranges.make_range('ref', 100 - size, 100 + size)
+    self.config.min_windows_distance = size
+    self.assertEqual(
+        window_selector._candidates_to_windows(self.config, candidates, 'ref'),
+        [expected])
+
+  @parameterized.parameters(range(1, 20))
+  def test_candidates_to_windows_min_window_distance(self, distance):
+    candidates = {
+        # We one candidate at position 100 with a 5 count.
+        100:
+            5,
+        # We have another candidate at outside of our distance with a 5 count,
+        # so it should produce a candidate but not be joined with our our
+        # candidate at 100.
+        100 - 2 * distance:
+            5,
+        # Finally, we have another variant that is exactly distance away from
+        # 100. It should be joined with the candidate at 100 to produce a single
+        # larger window.
+        100 + distance:
+            5,
+    }
+    expected = [
+        # Our first window is for the 100 - 2 * distance one.
+        ranges.make_range('ref', 100 - 3 * distance, 100 - distance),
+        # Our second window starts at 100 (- distance for the window size) and
+        # ends at 100 + distance + distance (again for window size).
+        ranges.make_range('ref', 100 - distance, 100 + 2 * distance),
+    ]
+    self.config.min_windows_distance = distance
+    self.assertEqual(
+        window_selector._candidates_to_windows(self.config, candidates, 'ref'),
+        expected)
+
+  @parameterized.parameters(range(1, 20))
+  def test_candidates_to_windows_merged_close_candidates(self, distance):
+    # Create five candidates separated by exactly distance from each other:
+    # 100, 101, 102, 103, 104 for distance == 1
+    # 100, 102, 104, 106, 108 for distance == 2
+    candidates = {100 + i * distance: 5 for i in range(5)}
+    # Which should all be merged together into one giant window.
+    expected = [
+        ranges.make_range('ref', 100 - distance,
+                          max(candidates) + distance),
+    ]
+    self.config.min_windows_distance = distance
+    self.assertEqual(
+        window_selector._candidates_to_windows(self.config, candidates, 'ref'),
+        expected)
 
 
 if __name__ == '__main__':
