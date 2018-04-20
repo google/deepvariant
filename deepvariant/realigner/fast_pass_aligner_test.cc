87da8249d1bac3ccc1b73771dc96104739b920a9
Genomics team in Google Brain <no-reply@google.com>
2018-04-20 10:40:18 -0700

    - FastAlignReadsToHaplotypes implementation added. - AlignHaplotype implementation added. - FastAlignStrings implementation added.
    PiperOrigin-RevId: 193689247

--- deepvariant/realigner/fast_pass_aligner_test.cc
+++ deepvariant/realigner/fast_pass_aligner_test.cc
@@ -42,7 +42,7 @@ namespace learning {
 namespace genomics {
 namespace deepvariant {
 
-class Aligner2Test : public ::testing::Test {
+class FastPassAlignerTest : public ::testing::Test {
  protected:
   FastPassAligner aligner_;
 
@@ -53,7 +53,7 @@ class Aligner2Test : public ::testing::Test {
   }
 };
 
-TEST_F(Aligner2Test, ReadsIndexIntegrationTest) {
+TEST_F(FastPassAlignerTest, ReadsIndexIntegrationTest) {
   aligner_.set_reads({"AAACCC", "CTCTCT", "TGAGCTGAAG"});
 
   KmerIndexType expected_index = {
@@ -82,6 +82,134 @@ TEST_F(Aligner2Test, ReadsIndexIntegrationTest) {
 
   EXPECT_EQ(aligner_.GetKmerIndex(), expected_index);
 }
+
+// Test haplotype consists of read1 and read3. We check expected haplotype
+// score and all read alignments.
+TEST_F(FastPassAlignerTest, FastAlignReadsToHaplotypeTest) {
+  aligner_.set_reads({"AAACCC", "CTCTCT", "TGAGCTGAAG"});
+  aligner_.set_kmer_size(3);
+  aligner_.BuildIndex();
+
+  // Haplotype made from read 3 + TT + read 1
+  string haplotype = "TGAGCTGAAGTTAAACCC";
+  int haplotype_score = 0;
+  std::vector<ReadAlignment> read_scores(aligner_.get_reads().size());
+
+  // Expected values
+  std::vector<string> aligner_reads = aligner_.get_reads();
+  int expected_hap_score =
+      aligner_reads[2].length() * aligner_.get_match_score() +
+      aligner_reads[0].length() * aligner_.get_match_score();
+
+  std::vector<ReadAlignment> expected_read_scores(aligner_reads.size());
+  expected_read_scores[0] = ReadAlignment(
+      12, "6=", aligner_reads[0].length() * aligner_.get_match_score());
+  expected_read_scores[1] = ReadAlignment();
+  expected_read_scores[2] = ReadAlignment(
+      0, "10=", aligner_reads[2].length() * aligner_.get_match_score());
+
+  aligner_.FastAlignReadsToHaplotype(haplotype, &haplotype_score, &read_scores);
+  EXPECT_EQ(expected_hap_score, haplotype_score);
+  EXPECT_THAT(read_scores,
+              testing::UnorderedElementsAreArray(expected_read_scores));
+}
+
+// One of the reads only partially overlaps haplotype. In this case the read
+// has to be skipped.
+TEST_F(FastPassAlignerTest, FastAlignReadsToHaplotypePartialReadOverlapTest) {
+  aligner_.set_reads({"AAACCC", "CTCTCT", "TGAGCTGAAG"});
+  aligner_.set_kmer_size(3);
+  aligner_.BuildIndex();
+
+  // Haplotype made from read 3 + TT + read first 4 bases of read 1
+  // In this case we cannot align read as a while and skip it.
+  string haplotype = "TGAGCTGAAGTTAAAC";
+  int haplotype_score = 0;
+  std::vector<ReadAlignment> read_scores(aligner_.get_reads().size());
+
+  // Expected values
+  std::vector<string> aligner_reads = aligner_.get_reads();
+  int expected_hap_score =
+      aligner_reads[2].length() * aligner_.get_match_score();
+
+  std::vector<ReadAlignment> expected_read_scores(aligner_reads.size());
+  expected_read_scores[0] = ReadAlignment();
+  expected_read_scores[1] = ReadAlignment();
+  expected_read_scores[2] = ReadAlignment(
+      0, "10=", aligner_reads[2].length() * aligner_.get_match_score());
+
+  aligner_.FastAlignReadsToHaplotype(haplotype, &haplotype_score, &read_scores);
+  EXPECT_EQ(expected_hap_score, haplotype_score);
+  EXPECT_THAT(read_scores,
+              testing::UnorderedElementsAreArray(expected_read_scores));
+}
+
+// One read is aligned with one mismatch.
+TEST_F(FastPassAlignerTest,
+       FastAlignReadsToHaplotypeReadAlignedWithMismatchesTest) {
+  aligner_.set_reads({"AAACCC", "CTCTCT", "TGAGCTGAAG"});
+  aligner_.set_kmer_size(3);
+  aligner_.BuildIndex();
+
+  // Haplotype made from read 3 with one mismatch + TT + read 1
+  string haplotype = "TGAGCCGAAGTTAAACCC";
+  int haplotype_score = 0;
+  std::vector<ReadAlignment> read_scores(aligner_.get_reads().size());
+
+  // Expected values
+  std::vector<string> aligner_reads = aligner_.get_reads();
+  int expected_hap_score =
+      (aligner_reads[2].length() - 1) * aligner_.get_match_score()
+      - 1 * aligner_.get_mismatch_penalty()
+      + aligner_reads[0].length() * aligner_.get_match_score();
+
+  std::vector<ReadAlignment> expected_read_scores(aligner_reads.size());
+  expected_read_scores[0] = ReadAlignment(
+      12, "6=", aligner_reads[0].length() * aligner_.get_match_score());
+  expected_read_scores[1] = ReadAlignment();
+  expected_read_scores[2] = ReadAlignment(
+      0,
+      "10=",
+      (aligner_reads[2].length() - 1) * aligner_.get_match_score()
+      - 1 * aligner_.get_mismatch_penalty());
+
+  aligner_.FastAlignReadsToHaplotype(haplotype, &haplotype_score, &read_scores);
+  EXPECT_EQ(expected_hap_score, haplotype_score);
+  EXPECT_THAT(read_scores,
+              testing::UnorderedElementsAreArray(expected_read_scores));
+}
+
+// One read is aligned with more than maximum allowed mismatches.
+TEST_F(FastPassAlignerTest,
+       FastAlignReadsToHaplotypeReadAlignedWithMoreThanMismatchesTest) {
+  aligner_.set_reads({"AAACCC", "CTCTCT", "TGAGCTGAAG"});
+  aligner_.set_kmer_size(3);
+  aligner_.set_max_num_of_mismatches(2);
+  aligner_.BuildIndex();
+
+  // Haplotype made from read 3 with 3 mismatches + TT + read 1
+  // max_num_of_mismatches_ is set to 2
+  string haplotype = "TTTGCCGAAGTTAAACCC";
+  int haplotype_score = 0;
+  std::vector<ReadAlignment> read_scores(aligner_.get_reads().size());
+
+  // Expected values
+  std::vector<string> aligner_reads = aligner_.get_reads();
+  int expected_hap_score =
+      aligner_reads[0].length() * aligner_.get_match_score();
+
+  std::vector<ReadAlignment> expected_read_scores(aligner_reads.size());
+  expected_read_scores[0] = ReadAlignment(
+      12, "6=", aligner_reads[0].length() * aligner_.get_match_score());
+  expected_read_scores[1] = ReadAlignment();
+  expected_read_scores[2] = ReadAlignment();
+
+  aligner_.FastAlignReadsToHaplotype(haplotype, &haplotype_score, &read_scores);
+  EXPECT_EQ(expected_hap_score, haplotype_score);
+  EXPECT_THAT(read_scores,
+              testing::UnorderedElementsAreArray(expected_read_scores));
+}
+
 
 }  // namespace deepvariant
 }  // namespace genomics
