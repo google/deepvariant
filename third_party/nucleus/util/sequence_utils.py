3ff0fddb0a69858bcf7d22ee2f77caf95bb3c9f3
Genomics team in Google Brain <no-reply@google.com>
2018-04-20 14:24:35 -0700

    PiperOrigin-RevId: 193722610

---
+++ third_party/nucleus/util/sequence_utils.py
@@ -0,0 +1,102 @@
+# Copyright 2018 Google Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""Utility functions for manipulating DNA sequences."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+
+class Error(Exception):
+  pass
+
+
+def _add_lowercase(d):
+  """Returns a dictionary with the lowercase keys and values entered."""
+  retval = d.copy()
+  retval.update({k.lower(): v.lower() for k, v in d.items()})
+  return retval
+
+
+STRICT_DNA_COMPLEMENT_UPPER = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+DNA_COMPLEMENT_UPPER = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C', 'N': 'N'}
+IUPAC_DNA_COMPLEMENT_UPPER = {
+    'A': 'T',
+    'T': 'A',
+    'C': 'G',
+    'G': 'C',
+    'R': 'Y',  # R is A/G
+    'Y': 'R',  # Y is C/T
+    'S': 'S',  # S is C/G
+    'W': 'W',  # W is A/T
+    'K': 'M',  # K is G/T
+    'M': 'K',  # M is A/C
+    'B': 'V',  # B is C/G/T
+    'V': 'B',  # V is A/C/G
+    'D': 'H',  # D is A/G/T
+    'H': 'D',  # H is A/C/T
+    'N': 'N',  # N is any base
+}
+
+STRICT_DNA_COMPLEMENT = _add_lowercase(STRICT_DNA_COMPLEMENT_UPPER)
+DNA_COMPLEMENT = _add_lowercase(DNA_COMPLEMENT_UPPER)
+IUPAC_DNA_COMPLEMENT = _add_lowercase(IUPAC_DNA_COMPLEMENT_UPPER)
+
+
+STRICT_DNA_BASES_UPPER = frozenset(['A', 'C', 'G', 'T'])
+STRICT_DNA_BASES = frozenset(['a', 'c', 'g', 't', 'A', 'C', 'G', 'T'])
+DNA_BASES_UPPER = frozenset(['A', 'C', 'G', 'T', 'N'])
+DNA_BASES = frozenset(['a', 'c', 'g', 't', 'n', 'A', 'C', 'G', 'T', 'N'])
+
+
+def reverse_complement(sequence, complement_dict=None):
+  """Returns the reverse complement of a DNA sequence.
+
+  By default this will successfully reverse complement sequences comprised
+  solely of A, C, G, and T letters. Other complement dictionaries can be
+  passed in for more permissive matching.
+
+  Args:
+    sequence: The input sequence to reverse complement.
+    complement_dict: The lookup dictionary holding the complement base pairs.
+
+  Returns:
+    The reverse complement DNA sequence.
+
+  Raises:
+    Error: The sequence contains letters not present in complement_dict.
+  """
+  if complement_dict is None:
+    complement_dict = STRICT_DNA_COMPLEMENT_UPPER
+
+  try:
+    return ''.join(complement_dict[nt] for nt in reversed(sequence))
+  except KeyError:
+    raise Error('Unknown base in %s, cannot reverse complement using %s' %
+                (sequence, str(complement_dict)))
---
+++ third_party/nucleus/util/sequence_utils_test.py
@@ -0,0 +1,107 @@
+# Copyright 2018 Google Inc.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from this
+#    software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+"""Tests for third_party.nucleus.util.sequence_utils."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from absl.testing import absltest
+
+from absl.testing import parameterized
+
+from third_party.nucleus.util import sequence_utils
+
+
+class SequenceUtilsTests(parameterized.TestCase):
+
+  @parameterized.parameters(
+      dict(seq='', expected=''),
+      dict(seq='A', expected='T'),
+      dict(seq='T', expected='A'),
+      dict(seq='C', expected='G'),
+      dict(seq='G', expected='C'),
+      dict(seq='GGGCAGATT', expected='AATCTGCCC'),
+      dict(
+          seq='GGGCAGANN',
+          expected='NNTCTGCCC',
+          complement_dict=sequence_utils.DNA_COMPLEMENT_UPPER),
+      dict(
+          seq='accgt',
+          expected='acggt',
+          complement_dict=sequence_utils.DNA_COMPLEMENT),
+      dict(
+          seq='ATCGRYSWKMBVDHN',
+          expected='NDHBVKMWSRYCGAT',
+          complement_dict=sequence_utils.IUPAC_DNA_COMPLEMENT_UPPER),
+      dict(
+          seq='ATCGRYSWKMBVDHNatcgryswkmbvdhn',
+          expected='ndhbvkmwsrycgatNDHBVKMWSRYCGAT',
+          complement_dict=sequence_utils.IUPAC_DNA_COMPLEMENT),
+  )
+  def test_reverse_complement(self, seq, expected, complement_dict=None):
+    """Tests canonical DNA sequences are reverse complemented correctly."""
+    self.assertEqual(
+        sequence_utils.reverse_complement(seq, complement_dict), expected)
+
+  @parameterized.parameters(
+      dict(seq='GGGCAGANN'),
+      dict(seq='accgt'),
+      dict(
+          seq='ATCGRYSWKMBVDHNatcgryswkmbvdhn',
+          complement_dict=sequence_utils.IUPAC_DNA_COMPLEMENT_UPPER),
+      dict(seq='X', complement_dict=sequence_utils.IUPAC_DNA_COMPLEMENT),
+  )
+  def test_bad_reverse_complement(self, seq, complement_dict=None):
+    """Tests error is raised when complement_dict does not cover given seq."""
+    with self.assertRaisesRegexp(sequence_utils.Error, 'Unknown base in'):
+      sequence_utils.reverse_complement(seq, complement_dict)
+
+  @parameterized.parameters(
+      dict(
+          bases_set=sequence_utils.STRICT_DNA_BASES_UPPER,
+          complement_dict=sequence_utils.STRICT_DNA_COMPLEMENT_UPPER),
+      dict(
+          bases_set=sequence_utils.STRICT_DNA_BASES,
+          complement_dict=sequence_utils.STRICT_DNA_COMPLEMENT),
+      dict(
+          bases_set=sequence_utils.DNA_BASES_UPPER,
+          complement_dict=sequence_utils.DNA_COMPLEMENT_UPPER),
+      dict(
+          bases_set=sequence_utils.DNA_BASES,
+          complement_dict=sequence_utils.DNA_COMPLEMENT),
+  )
+  def test_base_set_definitions(self, bases_set, complement_dict):
+    """Tests that base set and complement dict definitions are consistent."""
+    self.assertEqual(bases_set, frozenset(complement_dict.keys()))
+
+
+if __name__ == '__main__':
+  absltest.main()
