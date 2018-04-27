ff71fdabe6c5f62df1270037fb846663957d4de7
mdepristo <mdepristo@google.com>
2018-04-27 13:26:31 -0400

    Remove Python implementation of window_selector.
    PiperOrigin-RevId: 194559278

--- deepvariant/realigner/window_selector.py
+++ deepvariant/realigner/window_selector.py
@@ -32,124 +32,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import collections
-
-from absl import logging
-
-from third_party.nucleus.protos import cigar_pb2
 from third_party.nucleus.protos import reads_pb2
 from third_party.nucleus.util import ranges
 from deepvariant.protos import deepvariant_pb2
 from deepvariant.python import allelecounter
-from deepvariant.realigner import utils
 from deepvariant.realigner.python import window_selector as cpp_window_selector
-from deepvariant.vendor import timer
-
-# redacted
-# If True, we will run both the C++ and Python versions of the window selector
-# and compare the results.
-_COMPARE_CPP_PY = False
-
-# redacted
-# If True, we will use the C++ version of the window selector code. If False,
-# we will use the original Python version.
-_USE_CPP_IMPLEMENTATION = True
-
-
-def _process_aligned_bases(config, cigar, ref, read, ref_pos, read_pos):
-  for _ in range(cigar.operation_length):
-    # Check for ref_pos ranges, if the read is partially overlapped with
-    # ref sequence, it might be out-of-range and result in index error.
-    if ref_pos >= len(ref):
-      break
-    if ref_pos >= 0:
-      if ref[ref_pos] != read.aligned_sequence[read_pos]:
-        if read.aligned_quality[read_pos] >= config.min_base_quality:
-          yield ref_pos
-    read_pos += 1
-    ref_pos += 1
-
-
-def _process_insert_soft_clip(config, cigar, ref, read, ref_pos, read_pos):
-  del ref  # Unused in processing cigar insert operation.
-  if all(read.aligned_quality[read_pos + i] >= config.min_base_quality
-         for i in range(cigar.operation_length)):
-    for i in range(cigar.operation_length):
-      yield ref_pos + i
-      yield ref_pos - cigar.operation_length + i
-
-
-def _process_delete(config, cigar, ref, read, ref_pos, read_pos):
-  # Unused in processing cigar delete operation.
-  del config, ref, read, read_pos
-  for _ in range(cigar.operation_length):
-    yield ref_pos
-    ref_pos += 1
-
-
-def _process_read(config, ref, read, region):
-  """Yields reference positions corresponding to read's variations.
-
-  See _candidates_from_reads for more details on this algorithm.
-
-  Args:
-    config: learning.genomics.deepvariant.realigner.WindowSelectorOptions
-      options determining the behavior of this window selector.
-    ref: reference sequence
-    read: A genomics.Read record.
-    region: nucleus.protos.Range. The region we are constructing windows in.
-
-  Yields:
-    A tuple of (reference_position, count). The reference positions is within
-    the range of [0, seq_len], corresponding to read's variations, and count is
-    the number of "non-reference" events that occurred at that position.
-
-  Raises:
-    ValueError: for unsupported cigar operation.
-  """
-
-  if read.alignment.mapping_quality < config.min_mapq:
-    return
-
-  region_size = region.end - region.start
-  ref_pos = read.alignment.position.position - region.start
-  read_pos = 0
-  positions = []
-  for cigar in read.alignment.cigar:
-    # Break if it reached the end of reference sequence.
-    if ref_pos >= region_size:
-      break
-    if cigar.operation not in utils.CIGAR_OPS:
-      raise ValueError('Unexpected CIGAR operation', cigar, read)
-
-    if cigar.operation in {
-        cigar_pb2.CigarUnit.ALIGNMENT_MATCH, cigar_pb2.CigarUnit.SEQUENCE_MATCH,
-        cigar_pb2.CigarUnit.SEQUENCE_MISMATCH
-    }:
-      positions.extend(
-          _process_aligned_bases(config, cigar, ref, read, ref_pos, read_pos))
-      read_pos += cigar.operation_length
-      ref_pos += cigar.operation_length
-    elif cigar.operation in {
-        cigar_pb2.CigarUnit.INSERT, cigar_pb2.CigarUnit.CLIP_SOFT
-    }:
-      positions.extend(
-          _process_insert_soft_clip(config, cigar, ref, read, ref_pos,
-                                    read_pos))
-      read_pos += cigar.operation_length
-    elif cigar.operation == cigar_pb2.CigarUnit.DELETE:
-      positions.extend(
-          _process_delete(config, cigar, ref, read, ref_pos, read_pos))
-      ref_pos += cigar.operation_length
-    elif (cigar.operation == cigar_pb2.CigarUnit.CLIP_HARD or
-          cigar.operation == cigar_pb2.CigarUnit.PAD or
-          cigar.operation == cigar_pb2.CigarUnit.SKIP):
-      pass
-
-  # Yield positions within the range.
-  for pos, count in collections.Counter(positions).iteritems():
-    if pos >= 0 and pos < region_size:
-      yield pos + region.start, count
 
 
 def _candidates_from_reads(config, ref_reader, reads, region):
@@ -183,64 +70,6 @@ def _candidates_from_reads(config, ref_reader, reads, region):
     position summed up over all reads. The dictionary doesn't contain any
     position keys that would have a value of 0 (i.e., it's sparse).
   """
-  if _COMPARE_CPP_PY or _USE_CPP_IMPLEMENTATION:
-    with timer.Timer() as t2:
-      cpp_candidates = _candidates_from_reads_cpp(config, ref_reader, reads,
-                                                  region)
-      candidates = cpp_candidates
-
-  if _COMPARE_CPP_PY or not _USE_CPP_IMPLEMENTATION:
-    with timer.Timer() as t1:
-      python_candidates = _candidates_from_reads_python(config, ref_reader,
-                                                        reads, region)
-      if not _USE_CPP_IMPLEMENTATION:
-        candidates = python_candidates
-
-  if _COMPARE_CPP_PY:
-    logging.info('Region %s', ranges.to_literal(region))
-    logging.info('N. reads %s', len(reads))
-    logging.info('Python candidates %s', python_candidates)
-    logging.info('C++    candidates %s', cpp_candidates)
-    logging.info('Speed up: python: %5f, c++ %5f, relative cost %2f%%',
-                 t1.GetDuration(), t2.GetDuration(),
-                 (100.0 * t2.GetDuration()) / t1.GetDuration())
-    if python_candidates != cpp_candidates:
-      s = _show_diff(python_candidates, cpp_candidates)
-      # raise ValueError('Python and C++ candidates are different: %s' % s)
-      logging.warn('Python and C++ candidates are different: %s', s)
-
-  return candidates
-
-
-# redacted
-def _show_diff(py_candidates, cpp_candidates):
-  keys = set(py_candidates) | set(cpp_candidates)
-  result = {}
-  for key in keys:
-    pyr = py_candidates.get(key)
-    cppr = cpp_candidates.get(key)
-    result[key] = (pyr, cppr, '*** BAD' if pyr != cppr else '')
-  return '\n'.join('@{}: py={} cpp={} {}'.format(k, p, c, d)
-                   for k, (p, c, d) in sorted(result.iteritems()))
-
-
-# redacted
-def _candidates_from_reads_python(config, ref_reader, reads, region):
-  """See candidates_from_reads."""
-  # A list of candidate positions mapping to their number of supporting reads.
-  candidates = collections.defaultdict(int)
-
-  ref = ref_reader.query(region)
-  for read in reads:
-    for ref_pos, count in _process_read(config, ref, read, region):
-      candidates[ref_pos] += count
-
-  return candidates
-
-
-# redacted
-def _candidates_from_reads_cpp(config, ref_reader, reads, region):
-  """See candidates_from_reads."""
   allele_counter_options = deepvariant_pb2.AlleleCounterOptions(
       read_requirements=reads_pb2.ReadRequirements(
           min_mapping_quality=config.min_mapq,
