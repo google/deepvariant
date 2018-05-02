b756fc862611d54d4e42a2d09c89fdd8a55f683d
Genomics team in Google Brain <no-reply@google.com>
2018-05-02 15:01:53 -0700

    Fix the java installation in the DeepVariant OSS prereqs, which is currently failing on Debian.
    PiperOrigin-RevId: 195157793

--- build-prereq.sh
+++ build-prereq.sh
@@ -65,7 +65,8 @@ if ! java -version 2>&1 | fgrep "1.8"; then
   [[ $(lsb_release -d | grep 'Debian') ]] && \
     sudo -H apt-get install -y gnupg dirmngr && \
     sudo -H apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
-  sudo add-apt-repository -y ppa:webupd8team/java
+  echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+  echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
   sudo -H apt-get -qq -y update
   echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
   sudo -H apt-get install -y oracle-java8-installer
