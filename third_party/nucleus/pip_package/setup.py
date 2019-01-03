bbbc551961179c9640e3e85df937019bf013ce5a
Genomics team in Google Brain <no-reply@google.com>
2019-01-03 09:10:10 -0800

    remove opensource@google.com as author email for python package
    PiperOrigin-RevId: 227698296

--- third_party/nucleus/pip_package/setup.py
+++ third_party/nucleus/pip_package/setup.py
@@ -81,7 +81,6 @@ TensorFlow tfrecords file may be substituted.
 """,
     url='https://github.com/google/nucleus',
     author='The Genomics team in Google Brain',
-    author_email='opensource@google.com',
     license='Apache 2.0',
 
     # Taken from list of valid classifiers at
