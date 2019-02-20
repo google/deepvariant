72d59d24879b6369e9bf022fc7b6ccaf138ba516
koles <koles@google.com>
2019-02-20 10:38:57 -0800

    Allow to choose different model for tests. In addition some refactoring is done to make the test better structured and better compatible with different Linux distributions.
    PiperOrigin-RevId: 234824927

--- scripts/run_wgs_case_study_binaries.sh
+++ scripts/run_wgs_case_study_binaries.sh
@@ -3,18 +3,12 @@
 
 set -euo pipefail
 
-# Build binaries.
-# If you're using the pre-built binaries, you can skip these and just run
-# ./run-prereq.sh instead. And update the script to point to your *zip binaries.
-./build-prereq.sh
-./build_release_binaries.sh
-
 ## Preliminaries
 # Set a number of shell variables, to make what follows easier to read.
 BASE="${HOME}/case-study"
 MODEL_VERSION="0.7.2"
 MODEL_NAME="DeepVariant-inception_v3-${MODEL_VERSION}+data-wgs_standard"
-MODEL_HTTP_DIR="https://storage.googleapis.com/deepvariant/models/DeepVariant/${MODEL_VERSION}/${MODEL_NAME}"
+DEFAULT_MODEL_HTTP_DIR="https://storage.googleapis.com/deepvariant/models/DeepVariant/${MODEL_VERSION}/${MODEL_NAME}"
 
 INPUT_DIR="${BASE}/input"
 MODELS_DIR="${INPUT_DIR}/models"
@@ -35,88 +29,114 @@ OUTPUT_VCF="${OUTPUT_DIR}/HG002.output.vcf.gz"
 OUTPUT_GVCF="${OUTPUT_DIR}/HG002.output.g.vcf.gz"
 LOG_DIR="${OUTPUT_DIR}/logs"
 
-## Create local directory structure
-mkdir -p "${OUTPUT_DIR}"
-mkdir -p "${DATA_DIR}"
-mkdir -p "${MODELS_DIR}"
-mkdir -p "${LOG_DIR}"
+# Build binaries.
+# If you're using the pre-built binaries, you can skip these and just run
+# ./run-prereq.sh instead. And update the script to point to your *zip binaries.
+function build_binaries() {
+  ./build-prereq.sh
+  ./build_release_binaries.sh
+}
+
+function setup_test() {
+  ## Create local directory structure
+  mkdir -p "${OUTPUT_DIR}"
+  mkdir -p "${DATA_DIR}"
+  mkdir -p "${MODELS_DIR}"
+  mkdir -p "${LOG_DIR}"
 
 
-## Download extra packages
-# There are some extra programs we will need.
-# We are going to use [GNU Parallel](https://www.gnu.org/software/parallel/) to
-# run `make_examples`. We are going to install `samtools` and `docker.io` to help
-# do some analysis at the end.
-sudo apt-get -y update
-sudo apt-get -y install parallel
-sudo apt-get -y install samtools
-sudo apt-get -y install docker.io
-sudo apt-get -y install aria2
+  ## Download extra packages
+  # There are some extra programs we will need.
+  # We are going to use [GNU Parallel](https://www.gnu.org/software/parallel/) to
+  # run `make_examples`. We are going to install `samtools` and `docker.io` to help
+  # do some analysis at the end.
+  sudo apt-get -y update
+  sudo apt-get -y install parallel
+  sudo apt-get -y install samtools
+  sudo apt-get -y install docker.io
+  sudo apt-get -y install aria2
 
 
-## Download models, and test data
-# Copy the model files to your local disk.
-aria2c -c -x10 -s10 -d "${MODELS_DIR}" "${MODEL_HTTP_DIR}"/model.ckpt.data-00000-of-00001
-aria2c -c -x10 -s10 -d "${MODELS_DIR}" "${MODEL_HTTP_DIR}"/model.ckpt.index
-aria2c -c -x10 -s10 -d "${MODELS_DIR}" "${MODEL_HTTP_DIR}"/model.ckpt.meta
+  ## Download models, and test data
+  # Copy the model files to your local disk.
+  HTTP_ADDRESS="$http:\/\/.*"
+  GS_ADDRESS="$gs:\/\/.*"
+  if [[ $model_http_dir =~ $HTTP_ADDRESS ]];
+  then
+    aria2c -c -x10 -s10 -d "${MODELS_DIR}" "${model_http_dir}"/model.ckpt.data-00000-of-00001
+    aria2c -c -x10 -s10 -d "${MODELS_DIR}" "${model_http_dir}"/model.ckpt.index
+    aria2c -c -x10 -s10 -d "${MODELS_DIR}" "${model_http_dir}"/model.ckpt.meta
+  elif [[ $model_http_dir =~ $GS_ADDRESS ]];
+  then
+    gsutil cp "${model_http_dir}"/model.ckpt.data-00000-of-00001 "${MODELS_DIR}"
+    gsutil cp "${model_http_dir}"/model.ckpt.index "${MODELS_DIR}"
+    gsutil cp "${model_http_dir}"/model.ckpt.meta "${MODELS_DIR}"
+  else
+    echo 'Could not copy model. Unknown address prefix.'
+  fi
 
-# Copy the data
-aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/HG002_GRCh37_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-22_v.3.3.2_highconf_noinconsistent.bed -d "${DATA_DIR}"
-aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/HG002_GRCh37_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-22_v.3.3.2_highconf_triophased.vcf.gz -d "${DATA_DIR}"
-aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/HG002_GRCh37_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-22_v.3.3.2_highconf_triophased.vcf.gz.tbi -d "${DATA_DIR}"
-aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/HG002_NIST_150bp_50x.bam -d "${DATA_DIR}"
-aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/HG002_NIST_150bp_50x.bam.bai -d "${DATA_DIR}"
-aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/hs37d5.fa.gz -d "${DATA_DIR}"
-aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/hs37d5.fa.gz.fai -d "${DATA_DIR}"
-aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/hs37d5.fa.gz.gzi -d "${DATA_DIR}"
-aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/hs37d5.fa.gzi -d "${DATA_DIR}"
+  # Copy the data
+  aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/HG002_GRCh37_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-22_v.3.3.2_highconf_noinconsistent.bed -d "${DATA_DIR}"
+  aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/HG002_GRCh37_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-22_v.3.3.2_highconf_triophased.vcf.gz -d "${DATA_DIR}"
+  aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/HG002_GRCh37_GIAB_highconf_CG-IllFB-IllGATKHC-Ion-10X-SOLID_CHROM1-22_v.3.3.2_highconf_triophased.vcf.gz.tbi -d "${DATA_DIR}"
+  aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/HG002_NIST_150bp_50x.bam -d "${DATA_DIR}"
+  aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/HG002_NIST_150bp_50x.bam.bai -d "${DATA_DIR}"
+  aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/hs37d5.fa.gz -d "${DATA_DIR}"
+  aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/hs37d5.fa.gz.fai -d "${DATA_DIR}"
+  aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/hs37d5.fa.gz.gzi -d "${DATA_DIR}"
+  aria2c -c -x10 -s10 https://storage.googleapis.com/deepvariant/case-study-testdata/hs37d5.fa.gzi -d "${DATA_DIR}"
+}
 
 ## Run `make_examples`
-echo "Start running make_examples...Log will be in the terminal and also to ${LOG_DIR}/make_examples.log."
-( time seq 0 $((N_SHARDS-1)) | \
-  parallel -k --line-buffer \
-    python ./bazel-bin/deepvariant/make_examples.zip \
-      --mode calling \
-      --ref "${REF}" \
-      --reads "${BAM}" \
-      --examples "${EXAMPLES}" \
-      --gvcf "${GVCF_TFRECORDS}" \
-      --task {} \
-) 2>&1 | tee "${LOG_DIR}/make_examples.log"
-echo "Done."
-echo
+function run_make_examples() {
+  echo "Start running make_examples...Log will be in the terminal and also to ${LOG_DIR}/make_examples.log."
+  seq 0 $((N_SHARDS-1)) | \
+    parallel -k --line-buffer \
+      python ./bazel-bin/deepvariant/make_examples.zip \
+        --mode calling \
+        --ref "${REF}" \
+        --reads "${BAM}" \
+        --examples "${EXAMPLES}" \
+        --gvcf "${GVCF_TFRECORDS}" \
+        --task {}
+  echo "Done."
+  echo
+}
 
 ## Run `call_variants`
-echo "Start running call_variants...Log will be in the terminal and also to ${LOG_DIR}/call_variants.log."
-( time python ./bazel-bin/deepvariant/call_variants.zip \
-    --outfile "${CALL_VARIANTS_OUTPUT}" \
-    --examples "${EXAMPLES}" \
-    --checkpoint "${MODEL}"
-) 2>&1 | tee "${LOG_DIR}/call_variants.log"
-echo "Done."
-echo
+function run_call_variants() {
+  echo "Start running call_variants...Log will be in the terminal and also to ${LOG_DIR}/call_variants.log."
+  python ./bazel-bin/deepvariant/call_variants.zip \
+      --outfile "${CALL_VARIANTS_OUTPUT}" \
+      --examples "${EXAMPLES}" \
+      --checkpoint "${MODEL}"
+  echo "Done."
+  echo
+}
 
 ## Run `postprocess_variants`, without gVCFs.
-echo "Start running postprocess_variants (without gVCFs)...Log will be in the terminal and also to ${LOG_DIR}/postprocess_variants.log."
-( time python ./bazel-bin/deepvariant/postprocess_variants.zip \
-    --ref "${REF}" \
-    --infile "${CALL_VARIANTS_OUTPUT}" \
-    --outfile "${OUTPUT_VCF}"
-) 2>&1 | tee "${LOG_DIR}/postprocess_variants.log"
-echo "Done."
-echo
+function run_postprocess_variants() {
+  echo "Start running postprocess_variants (without gVCFs)...Log will be in the terminal and also to ${LOG_DIR}/postprocess_variants.log."
+  python ./bazel-bin/deepvariant/postprocess_variants.zip \
+      --ref "${REF}" \
+      --infile "${CALL_VARIANTS_OUTPUT}" \
+      --outfile "${OUTPUT_VCF}"
+  echo "Done."
+  echo
+}
 
 ## Run `postprocess_variants`, with gVCFs.
-echo "Start running postprocess_variants (with gVCFs)...Log will be in the terminal and also to ${LOG_DIR}/postprocess_variants.withGVCF.log."
-( time python ./bazel-bin/deepvariant/postprocess_variants.zip \
-    --ref "${REF}" \
-    --infile "${CALL_VARIANTS_OUTPUT}" \
-    --outfile "${OUTPUT_VCF}" \
-    --nonvariant_site_tfrecord_path "${GVCF_TFRECORDS}" \
-    --gvcf_outfile "${OUTPUT_GVCF}"
-) 2>&1 | tee "${LOG_DIR}/postprocess_variants.withGVCF.log"
-echo "Done."
-echo
+function run_postprocess_variants_gVCF() {
+  echo "Start running postprocess_variants (with gVCFs)...Log will be in the terminal and also to ${LOG_DIR}/postprocess_variants.withGVCF.log."
+  python ./bazel-bin/deepvariant/postprocess_variants.zip \
+      --ref "${REF}" \
+      --infile "${CALL_VARIANTS_OUTPUT}" \
+      --outfile "${OUTPUT_VCF}" \
+      --nonvariant_site_tfrecord_path "${GVCF_TFRECORDS}" \
+      --gvcf_outfile "${OUTPUT_GVCF}"
+  echo "Done."
+  echo
+}
 
 ## Evaluation: run hap.py
 echo "Start evaluation with hap.py..."
@@ -124,19 +144,36 @@ UNCOMPRESSED_REF="${OUTPUT_DIR}/hs37d5.fa"
 
 # hap.py cannot read the compressed fa, so uncompress
 # into a writable directory and index it.
-zcat <"${REF}" >"${UNCOMPRESSED_REF}"
-samtools faidx "${UNCOMPRESSED_REF}"
+function run_happy() {
+  zcat <"${REF}" >"${UNCOMPRESSED_REF}"
+  samtools faidx "${UNCOMPRESSED_REF}"
 
-sudo docker pull pkrusche/hap.py
-( sudo docker run -i \
--v "${DATA_DIR}:${DATA_DIR}" \
--v "${OUTPUT_DIR}:${OUTPUT_DIR}" \
-pkrusche/hap.py /opt/hap.py/bin/hap.py \
-  "${TRUTH_VCF}" \
-  "${OUTPUT_VCF}" \
-  -f "${TRUTH_BED}" \
-  -r "${UNCOMPRESSED_REF}" \
-  -o "${OUTPUT_DIR}/happy.output" \
-  --engine=vcfeval
-) 2>&1 | tee "${LOG_DIR}/happy.log"
-echo "Done."
+  sudo docker pull pkrusche/hap.py
+  sudo docker run -i \
+  -v "${DATA_DIR}:${DATA_DIR}" \
+  -v "${OUTPUT_DIR}:${OUTPUT_DIR}" \
+  pkrusche/hap.py /opt/hap.py/bin/hap.py \
+    "${TRUTH_VCF}" \
+    "${OUTPUT_VCF}" \
+    -f "${TRUTH_BED}" \
+    -r "${UNCOMPRESSED_REF}" \
+    -o "${OUTPUT_DIR}/happy.output" \
+    --engine=vcfeval
+  echo "Done."
+}
+
+function main() {
+  echo 'Starting the test...'
+  local -r model_http_dir="${1:-$DEFAULT_MODEL_HTTP_DIR}"
+  echo "Using model from: ${model_http_dir}"
+
+  build_binaries
+  setup_test
+  time run_make_examples 2>&1 | tee "${LOG_DIR}/make_examples.log"
+  time run_call_variants 2>&1 | tee "${LOG_DIR}/call_variants.log"
+  time run_postprocess_variants 2>&1 | tee "${LOG_DIR}/postprocess_variants.log"
+  time run_postprocess_variants_gVCF 2>&1 | tee "${LOG_DIR}/postprocess_variants.withGVCF.log"
+  run_happy 2>&1 | tee "${LOG_DIR}/happy.log"
+}
+
+main "$@"
