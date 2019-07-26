62337bd5807309012c5ee6e07ba482c53a156756
pichuan <pichuan@google.com>
2019-07-26 09:15:31 -0700

    Call build_release_binaries from build_and_test.sh so the fix can be applied more consistently.
    PiperOrigin-RevId: 260155784

--- build_and_test.sh
+++ build_and_test.sh
@@ -54,14 +54,9 @@ else
   bazel test -c opt ${DV_COPT_FLAGS} "$@" deepvariant/...
 fi
 
-# Create some build artifacts (two ways), and execute one of them.
-bazel build -c opt ${DV_COPT_FLAGS} "$@" deepvariant:binaries
+# Build the binary.
+./build_release_binaries.sh
+
 echo 'Expect a usage message:'
 (bazel-bin/deepvariant/call_variants --help || : ) | grep '/call_variants.py:'
 
-bazel build -c opt ${DV_COPT_FLAGS} "$@" deepvariant:binaries --build_python_zip
-echo 'Expect a usage message:'
-(bazel-bin/deepvariant/call_variants --help || : ) | grep '/call_variants.py:'
-
-# Bundle the licenses.
-bazel build :licenses_zip
