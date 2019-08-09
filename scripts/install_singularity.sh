5f0cb648a44ea119c5c92ccfefc8f84d7b018f62
pichuan <pichuan@google.com>
2019-08-09 10:55:06 -0700

    Add a convenient script for install Singularity. This is not currently used in our documentation, but might be useful later.
    PiperOrigin-RevId: 262591760

---
+++ scripts/install_singularity.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Copyright 2019 Google LLC.
+# This script is used to install `singularity` 3.3.0 on Ubutun 16.04.
+# For different Linux distributions and versions, modifications might be needed.
+# Installation instructions are from: https://sylabs.io/docs/
+
+set -euo pipefail
+
+sudo apt-get update && sudo apt-get install -y \
+  build-essential \
+  libssl-dev \
+  uuid-dev \
+  libgpgme11-dev \
+  squashfs-tools \
+  libseccomp-dev \
+  wget \
+  pkg-config \
+  git
+
+export VERSION=1.12 OS=linux ARCH=amd64
+# Downloads the required Go package
+wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
+# Extracts the archive
+sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
+# Deletes the ``tar`` file
+rm go$VERSION.$OS-$ARCH.tar.gz
+
+export VERSION=3.3.0
+wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz
+tar -xzf singularity-${VERSION}.tar.gz
+pushd singularity
+export PATH=/usr/local/go/bin:$PATH
+
+./mconfig
+make -C builddir
+sudo make -C builddir install
+
+# Returns to the original directory.
+popd
