cfe473f5476b090652d891f42a78928539023f3b
cym <cym@google.com>
2020-08-13 08:53:05 -0700

    Update setup.py to install .egg-info directory for pip 20.2+ compatibility.
    PiperOrigin-RevId: 326454661

--- third_party/nucleus/pip_package/setup.py
+++ third_party/nucleus/pip_package/setup.py
@@ -57,6 +57,13 @@ import os
 import shutil
 import sys
 
+# Basename of the .egg-info directory.
+_EGG_DIR_BASENAME = 'google_nucleus.egg-info'
+
+
+def _unsafe_filename(filename):
+  return any(c in filename for c in ' \t\n\r;&<>*')
+
 
 def touch(fname):
   open(fname, 'w+').close()
@@ -72,6 +79,27 @@ def find_destination(is_user):
     return install_cmd.install_platlib
 
 
+def copy_egg_info(dest_dir):
+  """Copies the .egg-info directory to the specified location.
+
+  Args:
+    dest_dir: str. The destination directory.
+
+  Returns:
+    0 on success, 1 on failure.
+  """
+  # Remove any trailing slash on the destination directory.
+  dest_dir = dest_dir.rstrip('/')
+  egg_srcs = glob.glob('google_nucleus-*-py*.egg-info')
+  if not egg_srcs:
+    print('Could not find source .egg-info directory')
+    return 1
+  egg_src = egg_srcs[0]
+  print('Copying egg-info from ', egg_src, ' to ', dest_dir)
+  shutil.copytree(egg_src, dest_dir)
+  return 0
+
+
 def main():
   if len(sys.argv) < 2:
     print('Missing command')
@@ -81,27 +109,21 @@ def main():
   args = sys.argv[2:]
 
   if cmd == 'egg_info':
-    egg_srcs = glob.glob('google_nucleus-*-py*.egg-info')
-    if not egg_srcs:
-      print('Could not find source .egg-info directory')
-      sys.exit(1)
-    egg_src = egg_srcs[0]
-
-    egg_dir = 'google_nucleus.egg-info'
+    egg_dir = _EGG_DIR_BASENAME
     if len(args) > 1 and args[0] == '--egg-base':
       egg_dir = os.path.join(args[1], egg_dir)
+    sys.exit(copy_egg_info(egg_dir))
 
-    print('Copying egg-info from ', egg_src, ' to ', egg_dir)
-    shutil.copytree(egg_src, egg_dir)
-    sys.exit(0)
-
-  if cmd == 'install':
+  elif cmd == 'install':
     destination = find_destination('--user' in args)
 
     record_file = 'install-record.txt'
     if '--record' in args:
       i = args.index('--record')
       record_file = args[i+1]
+      if _unsafe_filename(record_file):
+        print('Invalid record_file name', record_file)
+        sys.exit(1)
 
     print('Removing old protobuf files')
     os.system('rm -fR ' + destination + '/google/protobuf')
@@ -110,14 +132,17 @@ def main():
           + ' with record file at ' + record_file)
     os.system('cp -R -v google nucleus ' + destination
               + " | awk '{print substr($3,2,length($3)-2)}' > " + record_file)
+    # Append the .egg-info directory (must include trailing '/') to the record.
+    dest_egg_dir = os.path.join(destination, _EGG_DIR_BASENAME) + '/'
+    os.system('echo ' + dest_egg_dir + ' >> ' + record_file)
+    # End by copying the .egg-info directory to the destination.
+    sys.exit(copy_egg_info(dest_egg_dir))
 
-    sys.exit(0)
-
-  if cmd == 'bdist_wheel':
+  elif cmd == 'bdist_wheel':
     print('This package does not support wheel creation.')
     sys.exit(1)
 
-  if cmd == 'clean':
+  elif cmd == 'clean':
     # Nothing to do
     sys.exit(0)
 
@@ -127,4 +152,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-
