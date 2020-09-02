031115dcdde573828dce0c593fe0c408be4a0665
pichuan <pichuan@google.com>
2020-09-01 18:37:28 -0700

    Update version in docs and scripts.
    PiperOrigin-RevId: 329623302

--- scripts/run_wgs_case_study_binaries.sh
+++ scripts/run_wgs_case_study_binaries.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 ## Preliminaries
 # Set a number of shell variables, to make what follows easier to read.
 BASE="${HOME}/case-study"
-MODEL_VERSION="rc1.0.0"
+MODEL_VERSION="1.0.0"
 MODEL_NAME="DeepVariant-inception_v3-${MODEL_VERSION}+data-wgs_standard"
 DEFAULT_MODEL_HTTP_DIR="https://storage.googleapis.com/deepvariant/models/DeepVariant/${MODEL_VERSION}/${MODEL_NAME}"
 
