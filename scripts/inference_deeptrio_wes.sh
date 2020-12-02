eab9c4d88a1960f1a5e00b42de09e68e7c1abbd6
koles <koles@google.com>
2020-12-01 23:55:06 -0800

    Set binary version to 1.1.0 in DeepTrio inference scripts.
    PiperOrigin-RevId: 345176839

--- scripts/inference_deeptrio_wes.sh
+++ scripts/inference_deeptrio_wes.sh
@@ -99,7 +99,7 @@ echo "========================="
 ## Preliminaries
 # Set a number of shell variables, to make what follows easier to read.
 BASE="${HOME}/case-study"
-BIN_VERSION="1.1.0-rc20201118"
+BIN_VERSION="1.1.0"
 
 INPUT_DIR="${BASE}/input"
 MODELS_DIR="${INPUT_DIR}/models"
