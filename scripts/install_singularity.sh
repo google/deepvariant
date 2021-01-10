776c94f8c516a468b481b398899f5c0a192c7158
gunjanbaid <gunjanbaid@google.com>
2021-01-10 00:04:02 -0800

    Update singularity and go to latest versions for Ubuntu 18 compatibility.
    PiperOrigin-RevId: 350987987

--- scripts/install_singularity.sh
+++ scripts/install_singularity.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright 2019 Google LLC.
-# This script is used to install `singularity` 3.3.0 on Ubutun 18.04.
+# This script is used to install `singularity` 3.7.0 on Ubutun 18.04.
 # For different Linux distributions and versions, modifications might be needed.
 # Installation instructions are from: https://sylabs.io/docs/
 
@@ -17,7 +17,7 @@ sudo apt-get update && sudo apt-get install -y \
   pkg-config \
   git
 
-export VERSION=1.12 OS=linux ARCH=amd64
+export VERSION=1.15.6 OS=linux ARCH=amd64
 # Downloads the required Go package
 wget https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
 # Extracts the archive
@@ -25,7 +25,7 @@ sudo tar -C /usr/local -xzvf go$VERSION.$OS-$ARCH.tar.gz
 # Deletes the ``tar`` file
 rm go$VERSION.$OS-$ARCH.tar.gz
 
-export VERSION=3.3.0
+export VERSION=3.7.0
 wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz
 tar -xzf singularity-${VERSION}.tar.gz
 pushd singularity
