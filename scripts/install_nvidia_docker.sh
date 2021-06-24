32f40481239ff18bcd2402f851531c0c51caffaa
pichuan <pichuan@google.com>
2021-06-24 14:36:12 -0700

    Fix install_nvidia_docker.sh.
    PiperOrigin-RevId: 381339306

--- scripts/install_nvidia_docker.sh
+++ scripts/install_nvidia_docker.sh
@@ -66,7 +66,6 @@ sudo apt-get "${APT_ARGS[@]}" install docker-ce docker-ce-cli containerd.io
 distribution=$(. /etc/os-release;echo "$ID$VERSION_ID")
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L "https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list" | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-curl -s -L "https://nvidia.github.io/nvidia-container-runtime/experimental/$distribution/nvidia-container-runtime.list" | sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
 
 sudo apt-get update && sudo apt-get install "${APT_ARGS[@]}" nvidia-docker2
 sudo systemctl restart docker
