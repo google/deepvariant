945469d273cd2ceddbad553526a2c8952f53d52e
koles <koles@google.com>
2021-07-30 17:44:55 -0700

    Header for direct phasing class.
    PiperOrigin-RevId: 387915900

---
+++ deepvariant/direct_phasing.h
@@ -0,0 +1,145 @@
+/*
+ * Copyright 2021 Google LLC.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef LEARNING_GENOMICS_DEEPVARIANT_DIRECT_PHASING_H_
+#define LEARNING_GENOMICS_DEEPVARIANT_DIRECT_PHASING_H_
+
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <vector>
+
+#include "deepvariant/protos/deepvariant.pb.h"
+#include "absl/container/flat_hash_map.h"
+#include "absl/strings/string_view.h"
+#include "boost/graph/adjacency_list.hpp"
+#include "boost/graph/graph_traits.hpp"
+#include "third_party/nucleus/protos/reads.pb.h"
+#include "third_party/nucleus/util/proto_ptr.h"
+
+namespace learning {
+namespace genomics {
+namespace deepvariant {
+
+using uint16_t ReadIndex;
+
+// Data type storing read id and mapping quality. It is used in AlleleInfo.
+struct ReadSupportInfo {
+  ReadIndex read_index;
+  bool is_low_quality;
+};
+
+// Data type associated with graph nodes. It uniquely defines an allele by its
+// type and bases along with the vector of supporting read ids.
+struct AlleleInfo {
+  int position_ = 0;
+  std::string bases_ = "";
+  std::vector<ReadSupportInfo> read_support_;
+};
+
+struct VertexInfo {
+  AlleleInfo allele_info;
+};
+
+struct EdgeInfo {
+  float weight;
+};
+
+struct AlleleSupport{
+  Vertex vertex;
+  bool is_low_quality;
+};
+
+// Class that implements Direct Phasing algorithm. This class is only used by
+// make_examples.py. There are two exported methods:
+// * PhaseReads - called for each region and returns read phases calculated from
+//                candidates.
+// * GraphViz - auxiliary methods to create graphviz output for debugging
+//              purposes.
+class DirectPhasing {
+ public:
+  using Vertex = boost::graph_traits<BoostGraph>::vertex_descriptor;
+  using Edge = boost::graph_traits<BoostGraph>::edge_descriptor;
+  using RawVertexIndexMap = absl::flat_hash_map<Vertex, int>;
+  using VertexIndexMap =
+      boost::const_associative_property_map<RawVertexIndexMap>;
+
+  using BoostGraph = boost::adjacency_list<
+    boost::setS,            // Out edge list type.
+    boost::listS,           // Vertex list type.
+    boost::bidirectionalS,  // Directed graph.
+    VertexInfo,             // Vertex label.
+    EdgeInfo>;              // Edge label.
+
+  using VertexIterator = boost::graph_traits<BoostGraph>::vertex_iterator;
+  using EdgeIterator = boost::graph_traits<BoostGraph>::edge_iterator;
+  using AdjacencyIterator = boost::graph_traits<BoostGraph>::adjacency_iterator;
+
+  // Function returns read phases for each read in the input reads preserving
+  // the order. Python wrapper will be used to add phases to read protos in
+  // order to avoid copying gigabytes of memory.
+  std::vector<int> PhaseReads(
+      const std::vector<DeepVariantCall>& candidates,
+      const std::vector<
+          nucleus::ConstProtoPtr<const nucleus::genomics::v1::Read>>& reads
+      );
+
+  // Helper function to output graph into graphviz for debugging. This function
+  // is exported to Python.
+  std::string GraphViz() const;
+
+ private:
+  // Build graph from candidates.
+  void Build(
+      const std::vector<DeepVariantCall>& candidates,
+      const std::vector<
+          nucleus::ConstProtoPtr<const nucleus::genomics::v1::Read>>& reads);
+
+  BoostGraph graph_;
+  Vertex source_;
+  Vertex sink_;
+  RawVertexIndexMap vertex_index_map_;  // This is needed for GraphViz.
+
+  // Allele support for each read. Map is keyed by read id. Alleles are sorted
+  // by position. This map allows to quickly query all alleles that a read
+  // supports. Boolean variable designates if read to allele support is
+  // low_quality. If true then read supports the allele with low quality.
+  absl::flat_hash_map<std::string, std::vector<AlleleSupport>>
+      read_to_alleles_;
+  // Map read name to read id.
+  absl::flat_hash_map<std::string, ReadIndex> read_to_index;
+};
+
+}  // namespace deepvariant
+}  // namespace genomics
+}  // namespace learning
+
+#endif  // LEARNING_GENOMICS_DEEPVARIANT_DIRECT_PHASING_H_
