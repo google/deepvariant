8b62a12f0ebf630ce31e9793d952d0423b796522
pichuan <pichuan@google.com>
2023-02-10 13:13:41 -0800

    The default for phase_reads_region_padding_pct is already 20. Removing the flag from run_deeptrio.py for simplicity.
    PiperOrigin-RevId: 508737521

--- scripts/run_deeptrio.py
+++ scripts/run_deeptrio.py
@@ -390,7 +390,6 @@ def _make_examples_command(ref,
     special_args['vsc_min_fraction_indels'] = 0.12
     special_args['alt_aligned_pileup'] = 'diff_channels'
     special_args['add_hp_channel'] = True
-    special_args['phase_reads_region_padding_pct'] = 20
     special_args['min_mapping_quality'] = 1
     special_args['track_ref_reads'] = True
     special_args['pileup_image_height_child'] = (
