ce7a1f6d3ac925807c432aeb394c965c2f5c7d38
marianattestad <marianattestad@google.com>
2023-07-05 15:01:26 -0700

    Pin Pillow version to avoid API change affecting show_examples.
    PiperOrigin-RevId: 545795409

--- run-prereq.sh
+++ run-prereq.sh
@@ -146,7 +146,7 @@ pip3 install "${PIP_ARGS[@]}" 'pandas==1.2.4'
 # should also be updated accordingly.
 pip3 install "${PIP_ARGS[@]}" 'jsonschema==3.2.0'
 pip3 install "${PIP_ARGS[@]}" 'altair==4.1.0'
-pip3 install "${PIP_ARGS[@]}" 'Pillow>=5.4.1'
+pip3 install "${PIP_ARGS[@]}" 'Pillow==9.5.0'
 pip3 install "${PIP_ARGS[@]}" 'ipython>=7.9.0'
 pip3 install "${PIP_ARGS[@]}" 'pysam==0.20.0'
 
