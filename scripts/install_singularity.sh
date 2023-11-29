a58f762ae669da0e568dd6430eb80d7794b58cb9
koles <koles@google.com>
2023-11-28 16:18:01 -0800

    Internal change.
    PiperOrigin-RevId: 586134564

--- scripts/install_singularity.sh
+++ scripts/install_singularity.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2019 Google LLC.
+# Copyright 2023 Google LLC.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
