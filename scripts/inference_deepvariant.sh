b03416e9148825f99c01103d7f36b7b06c07dd3d
pichuan <pichuan@google.com>
2024-08-23 10:10:09 -0700

    Update version to latest.
    PiperOrigin-RevId: 666837034

--- scripts/inference_deepvariant.sh
+++ scripts/inference_deepvariant.sh
@@ -79,7 +79,7 @@ SKIP_HAPPY=false
 # Strings; sorted alphabetically.
 BAM=""
 DOCKER_SOURCE="google/deepvariant"
-BIN_VERSION="1.7.0"
+BIN_VERSION="latest"
 CALL_VARIANTS_ARGS=""
 CAPTURE_BED=""
 CUSTOMIZED_MODEL=""
