23d55cc3d4d62bb1ae6852918bcb95a3464b0bc8
lucasbrambrink <lucasbrambrink@google.com>
2024-12-12 16:12:59 -0800

    Internal change
    PiperOrigin-RevId: 705662661

--- deepvariant/small_model/small_model_config.py
+++ deepvariant/small_model/small_model_config.py
@@ -36,48 +36,26 @@ class PresetConfig(enum.Enum):
   """Enum of preset configs."""
 
   WGS = "wgs"
-  WES = "wes"
   PACBIO = "pacbio"
   ONT = "ont"
-  HYBRID = "hybrid"
 
 
 def set_wgs_config(config: ml_collections.ConfigDict) -> None:
-  config.train_tsv_directory = "/cns/oz-d/home/brain-genomics/lucasbrambrink/deepvariant_wgs/b356435536v2_wgs/ttl=14d/b356435536v2_wgs_train_ME_*.tfrecord.gz.small_model.tsv"
-  config.tune_tsv_directory = "/cns/oz-d/home/brain-genomics/lucasbrambrink/deepvariant_wgs/b356435536v2_wgs/ttl=14d/b356435536v2_wgs_tune_ME_*.tfrecord.gz.small_model.tsv"
-  config.num_train_samples = 887016302
-  config.num_tune_samples = 25218942
-
-
-def set_wes_config(config: ml_collections.ConfigDict) -> None:
-  config.train_tsv_directory = "/cns/oz-d/home/brain-genomics/lucasbrambrink/deepvariant_exome/b345306347_exome/ttl=14d/b345306347_exome_train_ME_*.tfrecord.gz.small_model.tsv"
-  config.tune_tsv_directory = "/cns/oz-d/home/brain-genomics/lucasbrambrink/deepvariant_exome/b345306347_exome/ttl=14d/b345306347_exome_tune_ME_*.tfrecord.gz.small_model.tsv"
-  config.num_train_samples = 19_217_144
-  config.num_tune_samples = 726_013
+  # TODO: Create examples for WGS.
+  config.model_params.expand_by_haplotype = False
 
 
 def set_pacbio_config(config: ml_collections.ConfigDict) -> None:
-  config.train_tsv_directory = "/cns/oz-d/home/brain-genomics/lucasbrambrink/deepvariant_pacbio/b366282091_pacbio/ttl=14d/b366282091_pacbio_train_ME_*.tfrecord.gz.small_model.tsv"
-  config.tune_tsv_directory = "/cns/oz-d/home/brain-genomics/lucasbrambrink/deepvariant_pacbio/b366282091_pacbio/ttl=14d/b366282091_pacbio_tune_ME_*.tfrecord.gz.small_model.tsv"
-  config.num_train_samples = 2968008448
-  config.num_tune_samples = 81352840
+  config.train_tfrecord_directory = "/placer/prod/home/brain-genomics/lucasbrambrink/deepvariant_pacbio/b382481037_pacbio/b382481037_pacbio_train_small_model.examples@100.tfrecord.gz"
+  config.tune_tfrecord_directory = "/placer/prod/home/brain-genomics/lucasbrambrink/deepvariant_pacbio/b382481037_pacbio/b382481037_pacbio_tune_small_model.examples@100.tfrecord.gz"
+  config.num_train_samples = 468223382
+  config.num_tune_samples = 14007034
   config.model_params.expand_by_haplotype = True
-  config.model_params.vaf_context_window_size = 51
-  config.model_params.hidden_layer_sizes = (750, 750)
 
 
 def set_ont_config(config: ml_collections.ConfigDict) -> None:
-  config.train_tsv_directory = "/cns/oz-d/home/brain-genomics/lucasbrambrink/deepvariant_ont/b356435536v2_ont/ttl=14d/b356435536v2_ont_train_ME_*.tfrecord.gz.small_model.tsv"
-  config.tune_tsv_directory = "/cns/oz-d/home/brain-genomics/lucasbrambrink/deepvariant_ont/b356435536v2_ont/ttl=14d/b356435536v2_ont_tune_ME_*.tfrecord.gz.small_model.tsv"
-  config.num_train_samples = 1374687650
-  config.num_tune_samples = 38297278
-
-
-def set_hybrid_config(config: ml_collections.ConfigDict) -> None:
-  config.train_tsv_directory = "/cns/oz-d/home/brain-genomics/lucasbrambrink/deepvariant_hybrid/b356435536v2_hybrid_pacbio_illumina/ttl=14d/b356435536v2_hybrid_pacbio_illumina_train_ME_*.tfrecord.gz.small_model.tsv"
-  config.tune_tsv_directory = "/cns/oz-d/home/brain-genomics/lucasbrambrink/deepvariant_hybrid/b356435536v2_hybrid_pacbio_illumina/ttl=14d/b356435536v2_hybrid_pacbio_illumina_tune_ME_*.tfrecord.gz.small_model.tsv"
-  config.num_train_samples = 198381199
-  config.num_tune_samples = 5406511
+  # TODO: Create examples for ONT.
+  config.model_params.expand_by_haplotype = True
 
 
 def set_preset_config(
@@ -86,14 +64,10 @@ def set_preset_config(
   """Sets the config for a given preset."""
   if preset_config == PresetConfig.WGS:
     set_wgs_config(config)
-  elif preset_config == PresetConfig.WES:
-    set_wes_config(config)
   elif preset_config == PresetConfig.PACBIO:
     set_pacbio_config(config)
   elif preset_config == PresetConfig.ONT:
     set_ont_config(config)
-  elif preset_config == PresetConfig.HYBRID:
-    set_hybrid_config(config)
 
 
 def get_config(config_name: str) -> ml_collections.ConfigDict:
@@ -101,18 +75,19 @@ def get_config(config_name: str) -> ml_collections.ConfigDict:
   # Model hyperparameters
   model_params = ml_collections.ConfigDict()
   model_params.activation = "relu"
-  model_params.hidden_layer_sizes = (500, 500)
+  model_params.hidden_layer_sizes = (750, 750)
   model_params.optimizer = "adam"
-  model_params.learning_rate = 0.0000001
+  model_params.learning_rate = 1e-04
   model_params.weight_decay = 0.0000001
   model_params.steps_per_execution = 128
   model_params.features = ()
-  model_params.vaf_context_window_size = 11
+  model_params.vaf_context_window_size = 51
   model_params.expand_by_haplotype = False
 
   # Training parameters
   config = ml_collections.ConfigDict()
-  config.epochs = 100
+  config.config_name = config_name
+  config.epochs = 50
   config.batch_size = 512
   config.logging_frequency = 16384
   config.interleave_cycle_length = 4
@@ -124,6 +99,7 @@ def get_config(config_name: str) -> ml_collections.ConfigDict:
   config.read_ahead_buffer = "128M"
   config.map_parallel_calls = 100
 
+  # Training data
   config.train_tfrecord_directory = ""
   config.num_train_samples = 0
   config.tune_tfrecord_directory = ""
