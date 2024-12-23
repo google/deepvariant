ced68d43b0d0da073ea0403d9474a34e57064e20
pichuan <pichuan@google.com>
2024-12-23 11:45:25 -0800

    Add -y to confirm.
    PiperOrigin-RevId: 709114288

--- scripts/inference_deepvariant.sh
+++ scripts/inference_deepvariant.sh
@@ -550,7 +550,7 @@ function setup_test() {
       gnupg-agent \
       software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository \
+    sudo add-apt-repository -y \
       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
       $(lsb_release -cs) \
       stable"
