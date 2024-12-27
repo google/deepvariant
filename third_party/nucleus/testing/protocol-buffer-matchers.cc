d343b216f4a312b8322216c4b38671f2f5d5f903
Genomics team in Google Health <no-reply@google.com>
2024-12-26 19:47:20 -0800

    Delete call sites of deprecated `proto2::util::MessageDifferencer::AddIgnoreCriteria(IgnoreCriteria*)` (pt 2 of 5).
    This change is one of many to remove call sites separately from main change cl/699223340.
    
    #busy-beavers #cppinliner #codehealth
    
    PiperOrigin-RevId: 709938561

--- third_party/nucleus/testing/protocol-buffer-matchers.cc
+++ third_party/nucleus/testing/protocol-buffer-matchers.cc
@@ -34,6 +34,7 @@
 #include <algorithm>
 #include <cstddef>
 #include <cstring>
+#include <memory>
 #include <sstream>
 #include <string>
 #include <vector>
@@ -282,7 +283,7 @@ void SetIgnoredFieldPathsOrDie(const google::protobuf::Descriptor& root_descript
                                const std::vector<string>& field_paths,
                                google::protobuf::util::MessageDifferencer* differencer) {
   for (const string& field_path : field_paths) {
-    differencer->AddIgnoreCriteria(new IgnoreFieldPathCriteria(
+    differencer->AddIgnoreCriteria(std::make_unique<IgnoreFieldPathCriteria>(
         ParseFieldPathOrDie(field_path, root_descriptor)));
   }
 }
