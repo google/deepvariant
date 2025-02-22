8576d53de6e485309d8243399caff3de45221902
pichuan <pichuan@google.com>
2025-02-22 08:49:05 -0800

    Addresses the issue of: ```       ERROR: setuptools==59.6.0 is used in combination with setuptools-scm>=8.x
          Your build configuration is incomplete and previously worked by accident!
          setuptools-scm requires setuptools>=61
    
          Suggested workaround if applicable:
           - migrating from the deprecated setup_requires mechanism to pep517/518
             and using a pyproject.toml to declare build dependencies
             which are reliably pre-installed before running the build tools
    ```
    
    PiperOrigin-RevId: 729890901

--- run-prereq.sh
+++ run-prereq.sh
@@ -149,6 +149,7 @@ pip3 install "${PIP_ARGS[@]}" 'Pillow==9.5.0'
 pip3 install "${PIP_ARGS[@]}" 'ipython==8.22.2'
 pip3 install "${PIP_ARGS[@]}" 'pysam==0.20.0'
 pip3 install "${PIP_ARGS[@]}" 'scikit-learn==1.0.2'
+pip3 install "${PIP_ARGS[@]}" 'setuptools==61.0.0'
 # This is to avoid ERROR: No matching distribution found for opencv-python-headless==4.5.2.52.
 # TODO: Make this the same as ${DV_GCP_OPTIMIZED_TF_WHL_VERSION}" later
 pip3 install "${PIP_ARGS[@]}"  "tf-models-official==2.13.1"
